398d729b52983f02bdabd59d4d4a8e230650f3dd
Christopher King <Chris.king@Keyin.com>
2025-11-05 16:25:04 -0330

    fix(dto): ignore unknown JSON fields in PassengerDto to stabilize tests

--- src/main/java/ca/keyin/cli/dto/PassengerDto.java
+++ src/main/java/ca/keyin/cli/dto/PassengerDto.java
@@ -1,3 +1,6 @@
 package ca.keyin.cli.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PassengerDto(Long id, String firstName, String lastName, String phoneNumber) {}
